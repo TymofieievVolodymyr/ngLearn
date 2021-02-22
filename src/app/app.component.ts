98157599064f83b416b3f4bef2146f47721c706b
Tymofieiev <vovanium.timofeev@gmail.com>
2021-02-22 21:39:37 +0200

    TD Creating the Form and Registering the Controls TD Submitting and Using the Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,17 @@
 import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  serverElements = [{type: 'server', name: 'TestServer', content: 'Just a test!'}];
-
-  onServerAdded(serverData: { serverName: string, serverContent: string }): void {
-    this.serverElements.push({
-      type: 'server',
-      name: serverData.serverName,
-      content: serverData.serverContent
-    });
+  suggestUserName(): void {
+    const suggestedName = 'Superuser';
   }
 
-  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }): void {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: blueprintData.serverName,
-      content: blueprintData.serverContent
-    });
-  }
-
-  onChangeFirst() {
-    this.serverElements[0].name = 'Changed!';
-  }
-
-  onDestroyFirst() {
-    this.serverElements.splice(0, 1);
+  onSubmit(form: NgForm): void {
+    console.log(form);
   }
 }

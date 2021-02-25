a77d75f9d7c32506065d75f83a5894e6dfa1c8b8
Tymofieiev <vovanium.timofeev@gmail.com>
2021-02-25 15:10:34 +0200

    transform response

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,12 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   loadedPosts = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }): void {
     this.http
@@ -25,10 +29,31 @@ export class AppComponent implements OnInit {
   }
 
   onFetchPosts(): void {
-    // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts(): void {
     // Send Http request
   }
+
+  private fetchPosts(): void {
+    this.http
+      .get('https://ng-vovanium-default-rtdb.firebaseio.com/posts.json')
+      .pipe(
+        map(responseData => {
+          const postsArray = [];
+          for (const key in responseData) {
+            if (responseData.hasOwnProperty(key)) {
+              postsArray.push({ ...responseData[key], id: key });
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe(posts => {
+        // ...
+        console.log(posts);
+      });
+  }
+
 }
